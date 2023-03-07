a6786ccbca4873b41eefd61f97ad0d4f27eb3667
naka520 <yashipipi0520@gmail.com>
2023-03-07 23:03:22 +0900

    "modify" profile

--- src/component/Page.js
+++ src/component/Page.js
@@ -1,11 +1,21 @@
+import { Stack } from "@mui/system";
 import React from "react";
 import "./../App.css";
 import Header from "./Header";
+import Pipi from "./pipi.png";
 
 function Page() {
   return (
     <div>
       <Header />
+      <div className="prof">
+        <ul className="profile">
+          <li>
+            <img src={Pipi}></img>
+          </li>
+          <li></li>
+        </ul>
+      </div>
     </div>
   );
 }
