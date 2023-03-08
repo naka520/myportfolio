bcddc946b4200afa7e726bd0d0c412e1cad4e0c8
naka520 <yashipipi0520@gmail.com>
2023-03-08 19:46:49 +0900

    "fix" tailwind

--- src/component/Header.js
+++ src/component/Header.js
@@ -4,13 +4,16 @@ import { AppBar, Toolbar, Typography } from "@mui/material";
 
 function Header() {
   return (
-    <AppBar position="static" color="default">
-      <Toolbar variant="dense">
-        <Typography variant="h6" color="inherit" component="div">
-          My Portfolio
-        </Typography>
-      </Toolbar>
-    </AppBar>
+    <div>
+      <AppBar position="static" color="default">
+        <Toolbar variant="dense" className="border-zinc-700">
+          <Typography variant="h6" color="inherit" component="div">
+            My Portfolio
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      {/* <div className="border-2">aiueo</div> */}
+    </div>
   );
 }
 
