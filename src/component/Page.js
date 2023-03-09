a78fa82633be46976dfa75fe8c2b792abcbdcc54
naka520 <yashipipi0520@gmail.com>
2023-03-10 00:26:06 +0900

    "add" 各種リンク追加

--- src/component/Page.js
+++ src/component/Page.js
@@ -1,4 +1,5 @@
 import { Stack } from "@mui/system";
+import { GitHubIcon } from "@mui/icons-material/GitHub";
 import React from "react";
 import "./../App.css";
 import Header from "./Header";
@@ -16,12 +17,31 @@ function Page() {
           <li className="border-2 h-4/5 w-4/5 rounded-tl-lg rounded-tr-lg rounded-br-lg rounded-bl-lg">
             <Stack>
               <ul className="mt-2  text-left text-xl">
-                <li>
+                <li className="mt-1 ml-2">
                   <p className="italic">Name : KaedeNakayashiki</p>
                 </li>
-                <li>
+                <li className="mt-1 ml-2">
                   <p className="italic">
-                    University : Kyushu Institute of Technology{" "}
+                    University : Kyushu Institute of Technology
+                  </p>
+                </li>
+                <li className="mt-1 ml-2">
+                  <p className="italic">
+                    GitHub ▶<a href="https://github.com/naka520">Click Me!</a>
+                  </p>
+                </li>
+                <li className="mt-1 ml-2">
+                  <p className="italic">
+                    Twitter ▶
+                    <a href="https://twitter.com/yashiki0520">Click Me!</a>
+                  </p>
+                </li>
+                <li className="mt-1 ml-2">
+                  <p className="italic">
+                    Wantedlly ▶
+                    <a href="https://www.wantedly.com/id/kaede_nakayashiki">
+                      Click Me!
+                    </a>
                   </p>
                 </li>
               </ul>
